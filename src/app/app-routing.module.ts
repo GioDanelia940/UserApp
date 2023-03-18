74db20236c8ac7c12301c02631af1605b8c5f87a
GioDanelia940 <108083792+GioDanelia940@users.noreply.github.com>
2023-03-19 02:23:24 +0400

    added card-item, main page, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserPageComponent } from './view/user-page/user-page.component';
 import { ViewComponent } from './view/view.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'view', pathMatch: 'full' },
-  { path: 'view', component: ViewComponent }
+  { path: 'view', component: ViewComponent },
+  {path:'view/:id',component:UserPageComponent}
 ];
 
 @NgModule({
--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpServiceService } from '../shared-services/http-service.service';
 
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
-  styleUrls: ['./view.component.css']
+  styleUrls: ['./view.component.css'],
 })
-export class ViewComponent {
-
+export class ViewComponent implements OnInit {
+  users: any[] = [];
+  constructor(private http: HttpServiceService) {}
+  ngOnInit(): void {
+    this.http.getAllUsers().subscribe((resp) => {
+      this.users = resp;
+    });
+  }
 }

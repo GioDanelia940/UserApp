0747a9d7a11af5887afd36c59471eb9ac41fb1d5
GioDanelia940 <108083792+GioDanelia940@users.noreply.github.com>
2023-03-19 02:57:35 +0400

    added user-page profile html, user object is taken with route param id and fetched from db.json

--- src/app/view/user-page/user-page.component.ts
+++ src/app/view/user-page/user-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HttpServiceService } from 'src/app/shared-services/http-service.service';
 
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
-  styleUrls: ['./user-page.component.css']
+  styleUrls: ['./user-page.component.css'],
 })
-export class UserPageComponent {
-
+export class UserPageComponent implements OnInit {
+  user!: any;
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpServiceService
+  ) {}
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.http.getUserById(params['id']).subscribe((resp) => {
+        this.user = resp;
+      });
+    });
+  }
 }
